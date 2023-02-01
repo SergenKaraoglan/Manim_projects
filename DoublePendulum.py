6b4e22729490da85b922f653e1ef2905d5532f13
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-02-01 12:16:03 +0000

    double pendulum logic complete

--- DoublePendulum.py
+++ DoublePendulum.py
@@ -4,10 +4,10 @@ from math import sin, cos, pi
 class DoublePendulum(Scene):
     def construct(self):
         # pendulum variables
-        g = 5
+        g = 9.8
         length1, length2 = 1, 1
-        mass1, mass2 = 1, 1
-        self.theta1, self.theta2 = pi/2, 0
+        mass1, mass2 = 2, 2
+        self.theta1, self.theta2 = pi/2, pi/2
         self.v1, self.v2 = 0, 0
 
         dotRadius = 0.16
@@ -25,14 +25,11 @@ class DoublePendulum(Scene):
 
         # pendulum animation
         def pendulum_updater(mobj, dt):
-            a1, a2 = self.calc(g, length1, length2, mass1, mass2)
-            #print(a1, a2)
-            self.v1 += a1
-            self.v2 += a2
-            print(self.v1, self.v2)
-            self.theta1 += self.v1
-            self.theta2 += self.v2
-            print(self.theta1)
+            a1, a2 = self.calc_accel(g, mass1, mass2, length1, length2)
+            self.v1 = (self.v1+a1*dt)#*0.999
+            self.v2 = (self.v2+a2*dt)#*0.999
+            self.theta1 += self.v1*dt
+            self.theta2 += self.v2*dt
             x1, y1, x2, y2 = self.get_position(length1, length2)
             l1.put_start_and_end_on([0,0,0], [x1, y1,0])
             p1.move_to([x1,y1,0])
@@ -43,23 +40,21 @@ class DoublePendulum(Scene):
         pendulum = VGroup(l1, p1, l2, p2)
         self.add(pendulum, originPoint)
         pendulum.add_updater(pendulum_updater)
-        self.wait(5)
+        self.wait(30)
 
+    def calc_accel(self, g, mass1, mass2, length1, length2):
+        num1=-g*(2*mass1+mass2)*sin(self.theta1)-mass2*g*sin(self.theta1-2*self.theta2)
+        num2=-2*sin(self.theta1-self.theta2)*mass2*(self.v2*self.v2*length2+self.v1*self.v1*length1*cos(self.theta1-self.theta2))
+        denominator=2*mass1+mass2-mass2*cos(2*self.theta2-2*self.theta1)
 
-    def calc(self, g, length1, length2, mass1, mass2):
-        numerator1 = -g * (2*mass1 + mass2) * sin(self.theta1) - mass2 * g * sin(self.theta1 - 2 * self.theta2)
-        - 2 * sin(self.theta1 - self.theta2)
-        numerator2 = numerator1 * mass2 * (self.v2**2 * length2 + self.v1**2 * length1 * cos(self.theta1-self.theta2))
-        denominator = length1 * (2 * mass1 + mass2 - mass2 * cos(2 * self.theta1 - 2 * self.theta2))
-        theta_pp1 = numerator2 / denominator
+        a1=(num1+num2)/(length1*denominator)
 
-        numerator = 2 * sin(self.theta1 - self.theta2) * (self.v1**2 * length1 * (mass1 + mass2) + g * (mass1 + mass2) 
-        * cos(self.theta1) + self.v2**2 * length2 * mass2 * cos(self.theta1 - self.theta2))
-        denominator = length2 * (2 * mass1 + mass2 - mass2 * cos(2 * self.theta1 - 2 * self.theta2))
-        theta_pp2 = numerator/ denominator
+        num1=self.v1*self.v1*length1*(mass1+mass2)+g*(mass1+mass2)*cos(self.theta1)
+        num2=self.v2*self.v2*length2*mass2*cos(self.theta1-self.theta2)
+        
+        a2=(2*sin(self.theta1-self.theta2)*(num1+num2))/(length2*denominator)
 
-        return theta_pp1, theta_pp2
-
+        return a1, a2
 
     def get_position(self, length1, length2):
         x1 = length1 * sin(self.theta1)
