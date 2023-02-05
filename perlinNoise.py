cf6bad2be2569b91601733d09a1922a838b5fe1f
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-02-05 16:01:24 +0000

    perlin noise update

--- perlinNoise.py
+++ perlinNoise.py
@@ -1,14 +1,28 @@
 from manim import *
-from math import sin, cos, pi
+from math import floor
 import random
 
 class PerlinNoise(Scene):
     def construct(self):
-         grid = [random.uniform(-2, 2) for _ in range(10)]
-         for i in range(len(grid)):
-            self.add(Dot([i-5, grid[i], 0]))
+         gradients = [random.uniform(-1, 1) for _ in range(10)]
+         
+         for i in range((len(gradients)-1)*10):
+            print('a')
+            x = i /10
+            y = self.sample(x, gradients)
+            print(y, x)
+            self.add(Dot([x - 5, (y+1)**2, 0]))
+         
+       
+
     
-    def lerp(alpha, y, y2):
-        return (1-alpha) * y + alpha * y2
-    def perlinNoise():
-        pass
+    def sample(self, x, gradients):
+        low = floor(x)
+        hi = low + 1
+        dist = x - low
+        lowSlope = gradients[low]
+        hiSlope = gradients[hi]
+        lowPos = lowSlope * dist
+        hiPos = -hiSlope * (1-dist)
+        u = dist * dist * (3.0 - 2.0 * dist)
+        return (lowPos*(1-u)) + (hiPos*u)
