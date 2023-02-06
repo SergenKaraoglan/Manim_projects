7bd261d8f77531d1400f7a1509fb00beec03cd74
Sergen Karaoglan <38327805+SergenKaraoglan@users.noreply.github.com>
2023-02-06 16:38:18 +0000

    change fractal tree stroke width per depth

--- FractalTree.py
+++ FractalTree.py
@@ -4,21 +4,22 @@ from math import pi, sin, cos
 class FractalTree(Scene):
     def construct(self):
         # fractal tree variables
-        angle = (2*pi / 11)/2
-        ratio = 0.75
+        angle = (pi/4)
+        ratio = 0.8
         length = 1.5
-        y = -4
+        y = -3
         x = 0
+        strokeWidth = 5
 
         # current and max depth of tree
         depth = 0
         maxDepth = 10
         # queue to contain lines (branches)
-        queue = [[Line([x,y,0], [x,y+length,0]), x, y+length, length*ratio, 0, depth]]
+        queue = [[Line([x,y,0], [x,y+length,0], stroke_width=strokeWidth), x, y+length, length*ratio, 0, depth, strokeWidth*ratio]]
         branches = [[] for _ in range(maxDepth)]
         while True:
             # get branch
-            line, x, y, length, angle2, depth = queue.pop(0)
+            line, x, y, length, angle2, depth, strokeWidth = queue.pop(0)
             # break condition
             if depth == maxDepth:
                 break
@@ -27,22 +28,22 @@ class FractalTree(Scene):
             branches[depth].append(line)
                     
             for i in range(2):
-                # get branch position
-                
                 # get inverse x coordinate for second branch
                 if i % 2:
                     x2, y2 = self.get_position(length, angle2 - angle)
-                    queue.append([Line([x, y, 0], [x + x2, y + y2, 0]), x + x2, y + y2, length * ratio, angle2 - angle, depth+1])
+                    queue.append([Line([x, y, 0], [x + x2, y + y2, 0], stroke_width=strokeWidth), 
+                                  x + x2, y + y2, length * ratio, angle2 - angle, depth+1, strokeWidth*ratio])
                 else:
                     x2, y2 = self.get_position(length, angle2 + angle)
-                    queue.append([Line([x, y, 0], [x + x2, y + y2, 0]), x + x2, y + y2, length * ratio, angle2 + angle, depth+1])
+                    queue.append([Line([x, y, 0], [x + x2, y + y2, 0], stroke_width=strokeWidth), 
+                                  x + x2, y + y2, length * ratio, angle2 + angle, depth+1, strokeWidth*ratio])
 
         # animate branches per depth
-        """ for i in range(len(branches)):
+        for i in range(len(branches)):
             branchGroup = VGroup(*branches[i])
-            self.play(*[Create(branch) for branch in branchGroup], run_time = 1.5) """
+            self.play(*[Create(branch) for branch in branchGroup], run_time = 1.5)
         # animate all branches at the same time
-        self.play(*[Create(branch) for i in range(maxDepth) for branch in branches[i]], run_time = 3)
+        #self.play(*[Create(branch) for i in range(maxDepth) for branch in branches[i]], run_time = 3)
 
 
     # get x, y coordinate using angle and length
